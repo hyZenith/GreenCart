cd3e7a102b1246597072c053527f5fe9e0d05a3b
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2025-10-05 13:35:37 +0530

    modified routes

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import {upload} from '../configs/multer.js';
-import {authSeller} from '../middlewares/authMiddleware.js';
+import authSeller from '../middlewares/authSeller.js';
 import {addProduct, productList, productById, ChangeStock} from '../controllers/productControler.js';
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.array([Images]), authSeller, addProduct);
+productRouter.post('/add',upload.array(["images"]), authSeller, addProduct);
 productRouter.get('/list', productList);
 productRouter.post('/id', productById);
 productRouter.post('/stock', authSeller, ChangeStock);
