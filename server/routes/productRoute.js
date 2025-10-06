9510f0912ff7d079f06074ed302b9e3dfb765cb2
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2025-10-06 22:48:31 +0530

    change in routes

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -4,7 +4,8 @@ import authSeller from '../middlewares/authSeller.js';
 import {addProduct, productList, productById, ChangeStock} from '../controllers/productControler.js';
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.array(["images"]), authSeller, addProduct);
+// accept up to 4 files under the 'images' field
+productRouter.post('/add', upload.array('images', 4), authSeller, addProduct);
 productRouter.get('/list', productList);
 productRouter.post('/id', productById);
 productRouter.post('/stock', authSeller, ChangeStock);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,6 +6,7 @@ const userRouter = express.Router();
 userRouter.post('/register', register )
 userRouter.post('/login', login )
 userRouter.get('/is-auth',authUser, isAuth )
-userRouter.get('/logout', authUser , logout )
+// Logout should clear cookie regardless of auth state so allow unauthenticated calls
+userRouter.get('/logout', logout )
 
 export default userRouter;
